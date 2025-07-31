77977e45e8c30614a958b525e51ec549c9751824
Pratik Suresh farate <pratikfarate33@gmail.com>
2025-07-31 15:07:47 +0530

    31/07/2025

---
+++ String.py
@@ -0,0 +1,13 @@
+# str="Hello"
+# print(str)
+
+# print(str[-5:])
+
+str="pratik farate"
+print(str[0])
+
+print(str.find("p"))
+print(str.replace("pra","tru"))
+print(str.count("t"))
+print(str.split())
+print(str[0].upper(),str[1:13].lower())
