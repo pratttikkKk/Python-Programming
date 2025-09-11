573ba50dbb538290596303f2b0c5d8def9612915
Pratik Suresh farate <pratikfarate33@gmail.com>
2025-09-11 15:24:02 +0530

    11/09/25

---
+++ Constructors/Default_constructors.py
@@ -0,0 +1,12 @@
+class Name:
+	def __init__(self):
+		
+		self.First_Name ="pratik"
+		self.Last_Name ="farate"
+		self.age = 23
+
+nm = Name()
+
+print(nm.First_Name)
+print(nm.Last_Name)
+print(nm.age)
---
+++ Constructors/parameterised_constructor.py
@@ -0,0 +1,26 @@
+class Name:
+	def __init__(self,First_Name,Last_Name,age):              
+		
+		self.F_Name =First_Name
+		self.L_Name =Last_Name
+		self.ag = age
+
+nm = Name("pratik","farate",21)
+
+print(nm.F_Name)
+print(nm.L_Name)
+print(nm.ag)
+
+
+# class Name:
+# 	def __init__(self):
+		
+# 		self.First_Name ="pratik"
+# 		self.Last_Name ="farate"
+# 		self.age = 23
+
+# nm = Name()
+
+# print(nm.First_Name)
+# print(nm.Last_Name)
+# print(nm.age)
