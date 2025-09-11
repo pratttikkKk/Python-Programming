4bd06529ee164c11bffef32fe918b4747a7fd411
Pratik Suresh farate <pratikfarate33@gmail.com>
2025-09-11 15:53:29 +0530

    11/09/25

--- Constructors/Default_constructors.py
+++ Constructors/Default_constructors.py
@@ -1,12 +1,14 @@
 class Name:
-	def __init__(self):
-		
-		self.First_Name ="pratik"
-		self.Last_Name ="farate"
-		self.age = 23
+    def __init__(self):
+        self.First_Name = "pratik"
+        self.Last_Name = "farate"
+        self.age = 23
 
+    def __del__(self):
+        print('Destructor called, Employee deleted.')
 nm = Name()
 
 print(nm.First_Name)
 print(nm.Last_Name)
 print(nm.age)
+del nm
